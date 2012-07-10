6257327f9b4c7763dadadf06a4b7f31f72ec2de8
Aydin Han <aydinhan@gmail.com>
2012-07-10 16:52:16 +0300

    fix typos

--- run/run-experiment.sh
+++ run/run-experiment.sh
@@ -1,2 +1,2 @@
 cd $1
-nohup ../../src/moses/scripts/ems/experiment.perl -multicore -max-active 10 -no-graph -config config -exec >& OUT.1 &
+nohup ../../src/moses/scripts/ems/experiment.perl -multicore -max-active 8 -no-graph -config config -exec >& OUT.1 &
