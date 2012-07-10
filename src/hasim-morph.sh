a9dba0748f416b9e7228595b3160af787324d116
Aydin Han <aydinhan@gmail.com>
2012-07-10 07:06:14 +0300

    updated scripts

--- src/hasim-morph.sh
+++ src/hasim-morph.sh
@@ -11,4 +11,4 @@ $DISAMB/md.pl -disamb $DISAMB/model.txt $1.mor.amb $1.mor.disamb
 sed -e 's/]+\[//g;s/]-\[//g;s/\[[^]]*\]//g' $1.mor.disamb > $1.tmp.1
 java -cp $DIR ParseHasimOutput $1.tmp.1 > $1.tmp.2
 cat $1.tmp.2 | tr '\n' ' ' | sed 's#<S> ##g;s# <\/S> #\n#g' > $1.mor
-rm $1.amb $1.disamb $1.tmp.*
+#rm $1.amb $1.disamb $1.tmp.*
--- src/tree-tagger.sh
+++ src/tree-tagger.sh
@@ -8,4 +8,4 @@ sed 's#^#<S> #g;s#$# <\/S>#g;s# #\n#g' $1 > $1.tmp.1
 $TT/bin/tree-tagger -token -lemma -sgml -no-unknown $TT/lib/english.par $1.tmp.1 $1.tmp.2
 java -cp $DIR ParseTreeTaggerOutput $1.tmp.2 > $1.tmp.3
 cat $1.tmp.3 | tr '\n' ' ' | sed 's#<S> ##g;s# <\/S> #\n#g' > $1.mor
-rm $1.tmp.*
+#rm $1.tmp.*
