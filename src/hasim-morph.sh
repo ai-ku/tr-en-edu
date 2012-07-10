d7ff7cf8f189f03f12488f8be00bfe733cd6ca44
Aydin Han <aydinhan@gmail.com>
2012-07-10 07:02:02 +0300

    updated scripts

--- src/hasim-morph.sh
+++ src/hasim-morph.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd $( dirname "$0" )
+DIR="$( dirname "$0" )"
 
-PARSER="hasim-morph/MP-1.0-Linux64"
-DISAMB="hasim-morph/MD-2.0"
+PARSER="$DIR/hasim-morph/MP-1.0-Linux64"
+DISAMB="$DIR/hasim-morph/MD-2.0"
 
 $PARSER/parse_corpus.py $PARSER/turkish.fst $1 > $1.mor.amb
 $DISAMB/md.pl -disamb $DISAMB/model.txt $1.mor.amb $1.mor.disamb
 
 sed -e 's/]+\[//g;s/]-\[//g;s/\[[^]]*\]//g' $1.mor.disamb > $1.tmp.1
-java -cp . ParseHasimOutput $1.tmp.1 > $1.tmp.2
+java -cp $DIR ParseHasimOutput $1.tmp.1 > $1.tmp.2
 cat $1.tmp.2 | tr '\n' ' ' | sed 's#<S> ##g;s# <\/S> #\n#g' > $1.mor
 rm $1.amb $1.disamb $1.tmp.*
--- src/tree-tagger.sh
+++ src/tree-tagger.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd $( dirname "$0" )
+DIR="$( dirname "$0" )"
 
-TT="tree-tagger"
+TT="$DIR/tree-tagger"
 
 sed 's#^#<S> #g;s#$# <\/S>#g;s# #\n#g' $1 > $1.tmp.1
 $TT/bin/tree-tagger -token -lemma -sgml -no-unknown $TT/lib/english.par $1.tmp.1 $1.tmp.2
-java -cp . ParseTreeTaggerOutput $1.tmp.2 > $1.tmp.3
+java -cp $DIR ParseTreeTaggerOutput $1.tmp.2 > $1.tmp.3
 cat $1.tmp.3 | tr '\n' ' ' | sed 's#<S> ##g;s# <\/S> #\n#g' > $1.mor
 rm $1.tmp.*
